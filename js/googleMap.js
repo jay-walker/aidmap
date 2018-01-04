360596236f6c382ce732ebea07e61edc7cb81291
jay-walker <mr.carandache@gmail.com>
2018-01-05 00:05:37 +0300

    changed folders structure

--- js/googleMap.js
+++ js/googleMap.js
@@ -3,6 +3,6 @@ function myMap() {
         center: new google.maps.LatLng(51.5, -0.12),
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
+        }
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+        }
